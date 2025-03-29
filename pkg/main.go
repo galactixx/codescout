044505b9b85a9cf0af37a7dc729bab0d9e9aea8e
galactixx <nwlezien.github@gmail.com>
2025-03-29 18:11:57 -0500

    refactor inspectors

--- pkg/main.go
+++ pkg/main.go
@@ -14,53 +14,142 @@ import (
 type Inspector interface {
 	nameEquals(name string) bool
 	appendNode(node BaseNode)
+	inspector(n ast.Node) bool
+	inspect(n ast.Node)
 }
 
-type BaseInspector struct {
+type baseInspector struct {
 	Path  string
 	Fset  *token.FileSet
 	Count int
 }
 
-func (i *BaseInspector) increment() {
+func (i *baseInspector) inspect(node ast.Node, inspector func(n ast.Node) bool) {
+	ast.Inspect(node, func(n ast.Node) bool {
+		if i.Count > 0 {
+			return false
+		}
+		return inspector(n)
+	})
+}
+
+func (i *baseInspector) newNode(name string, node ast.Node, comment string) BaseNode {
+	line, characters := i.getPos(node)
+	return BaseNode{
+		Name:       name,
+		Path:       i.Path,
+		Line:       line,
+		Characters: characters,
+		Exported:   token.IsExported(name),
+		Comment:    comment,
+	}
+}
+
+func (i *baseInspector) increment() {
 	i.Count += 1
 }
 
-func (i BaseInspector) getPos(node ast.Node) (int, int) {
+func (i *baseInspector) getPos(node ast.Node) (int, int) {
 	pos := i.Fset.Position(node.Pos())
 	line := pos.Line
 	characters := pos.Column
 	return line, characters
 }
 
-type StructInspector struct {
-	Nodes  *[]StructNode
+type structInspector struct {
+	Nodes  []StructNode
 	Config StructConfig
-	Base   BaseInspector
+	Base   baseInspector
 }
 
-func (i StructInspector) nameEquals(name string) bool {
+func (i *structInspector) nameEquals(name string) bool {
 	return i.Config.Name == name
 }
 
-func (i *StructInspector) appendNode(node StructNode) {
-	*i.Nodes = append(*i.Nodes, node)
+func (i *structInspector) appendNode(node StructNode) {
+	i.Nodes = append(i.Nodes, node)
 	i.Base.increment()
 }
 
-type FuncInspector struct {
-	Nodes  *[]FuncNode
+func (i *structInspector) inspect(node ast.Node) {
+	i.Base.inspect(node, i.inspector)
+}
+
+func (i *structInspector) newStruct(node ast.Node, spec *ast.TypeSpec) StructNode {
+	var comment string = ""
+	if spec.Doc != nil {
+		comment = spec.Doc.Text()
+	}
+	baseNode := i.Base.newNode(spec.Name.Name, node, comment)
+	structNode := node.(*ast.StructType)
+	return StructNode{Node: baseNode, node: structNode}
+}
+
+func (i *structInspector) inspector(node ast.Node) bool {
+	genDecl, ok := node.(*ast.GenDecl)
+	if !ok {
+		return true
+	}
+
+	for _, spec := range genDecl.Specs {
+		if typeSpec, ok := spec.(*ast.TypeSpec); ok {
+			if structType, ok := typeSpec.Type.(*ast.StructType); ok {
+				if i.nameEquals(typeSpec.Name.Name) {
+					structNode := i.newStruct(structType, typeSpec)
+					i.appendNode(structNode)
+				}
+			}
+		}
+	}
+	return true
+}
+
+type funcInspector struct {
+	Nodes  []FuncNode
 	Config FunctionConfig
-	Base   BaseInspector
+	Base   baseInspector
 }
 
-func (i FuncInspector) nameEquals(name string) bool {
+func (i *funcInspector) nameEquals(name string) bool {
 	return i.Config.Name == name
 }
 
-func (i *FuncInspector) appendNode(node FuncNode) {
-	*i.Nodes = append(*i.Nodes, node)
+func (i *funcInspector) appendNode(node FuncNode) {
+	i.Nodes = append(i.Nodes, node)
 	i.Base.increment()
+}
+
+func (i *funcInspector) inspect(node ast.Node) {
+	i.Base.inspect(node, i.inspector)
+}
+
+func (i *funcInspector) newFunction(name string, node ast.Node, comment string) FuncNode {
+	baseNode := i.Base.newNode(name, node, comment)
+	funcNode := node.(*ast.FuncDecl)
+	return FuncNode{Node: baseNode, node: funcNode}
+}
+
+func (i *funcInspector) inspector(n ast.Node) bool {
+	funcDecl, ok := n.(*ast.FuncDecl)
+	if !ok {
+		return true
+	}
+
+	if i.nameEquals(funcDecl.Name.Name) {
+		var comment string = ""
+		if funcDecl.Doc != nil {
+			comment = funcDecl.Doc.Text()
+		}
+		name := funcDecl.Name.Name
+		funcNode := i.newFunction(name, funcDecl, comment)
+		i.appendNode(funcNode)
+	}
+	return true
+}
+
+type Parameter struct {
+	Name string
+	Type string
 }
 
 type NodeInfo interface {
@@ -82,7 +171,18 @@ type FuncNode struct {
 	node *ast.FuncDecl
 }
 
-func (f FuncNode) Comments() string {
+func (f *FuncNode) Parameters() []Parameter {
+	parameters := make([]Parameter, 0, 5)
+	for _, parameter := range f.node.Type.Params.List {
+		for _, name := range parameter.Names {
+			parameter := Parameter{Name: name.Name, Type: nodeToCode(parameter.Type)}
+			parameters = append(parameters, parameter)
+		}
+	}
+	return parameters
+}
+
+func (f *FuncNode) Comments() string {
 	var buf bytes.Buffer
 	for _, comment := range f.node.Doc.List {
 		buf.WriteString(comment.Text + "\n")
@@ -90,7 +190,7 @@ func (f FuncNode) Comments() string {
 	return buf.String()
 }
 
-func (f FuncNode) Code() string {
+func (f *FuncNode) Code() string {
 	nodeOriginalDoc := f.node.Doc
 	f.node.Doc = nil
 	codeString := nodeToCode(f.node)
@@ -102,15 +202,15 @@ func (f FuncNode) Code() string {
 	}
 }
 
-func (f FuncNode) PrintNode() {
+func (f *FuncNode) PrintNode() {
 	fmt.Println(f.Code())
 }
 
-func (f FuncNode) Body() string {
+func (f *FuncNode) Body() string {
 	return nodeToCode(f.node.Body)
 }
 
-func (f FuncNode) Signature() string {
+func (f *FuncNode) Signature() string {
 	return nodeToCode(&ast.FuncDecl{
 		Name: f.node.Name,
 		Type: f.node.Type,
@@ -122,11 +222,11 @@ type StructNode struct {
 	node *ast.StructType
 }
 
-func (s StructNode) Code() string {
+func (s *StructNode) Code() string {
 	return nodeToCode(s.node)
 }
 
-func (s StructNode) PrintNode() {
+func (s *StructNode) PrintNode() {
 	fmt.Println(s.Code())
 }
 
@@ -134,77 +234,6 @@ func nodeToCode(node any) string {
 	var buf bytes.Buffer
 	printer.Fprint(&buf, token.NewFileSet(), node)
 	return buf.String()
-}
-
-func newFunction(name string, node ast.Node, insp FuncInspector, comment string) FuncNode {
-	baseNode := newNode(name, node, insp.Base, comment)
-	funcNode := node.(*ast.FuncDecl)
-	return FuncNode{Node: baseNode, node: funcNode}
-}
-
-func newStruct(node ast.Node, spec *ast.TypeSpec, insp StructInspector) StructNode {
-	var comment string = ""
-	if spec.Doc != nil {
-		comment = spec.Doc.Text()
-	}
-	baseNode := newNode(spec.Name.Name, node, insp.Base, comment)
-	structNode := node.(*ast.StructType)
-	return StructNode{Node: baseNode, node: structNode}
-}
-
-func newNode(
-	name string, node ast.Node, inspector BaseInspector, comment string,
-) BaseNode {
-	line, characters := inspector.getPos(node)
-	return BaseNode{
-		Name:       name,
-		Path:       inspector.Path,
-		Line:       line,
-		Characters: characters,
-		Exported:   token.IsExported(name),
-		Comment:    comment,
-	}
-}
-
-func functionInspector(n ast.Node, inspector FuncInspector) bool {
-	funcDecl, ok := n.(*ast.FuncDecl)
-	if !ok {
-		return true
-	}
-
-	if inspector.nameEquals(funcDecl.Name.Name) {
-		var comment string = ""
-		if funcDecl.Doc != nil {
-			comment = funcDecl.Doc.Text()
-		}
-		name := funcDecl.Name.Name
-		funcNode := newFunction(name, funcDecl, inspector, comment)
-		inspector.appendNode(funcNode)
-	}
-	return true
-}
-
-func structInspector(n ast.Node, inspector StructInspector) bool {
-	genDecl, ok := n.(*ast.GenDecl)
-	if !ok {
-		return true
-	}
-
-	for _, spec := range genDecl.Specs {
-		if typeSpec, ok := spec.(*ast.TypeSpec); ok {
-			if structType, ok := typeSpec.Type.(*ast.StructType); ok {
-				if inspector.nameEquals(typeSpec.Name.Name) {
-					structNode := newStruct(structType, typeSpec, inspector)
-					inspector.appendNode(structNode)
-				}
-			}
-		}
-	}
-	return true
-}
-
-func methodInsepector(n ast.Node, inspector Inspector) bool {
-	return true
 }
 
 func parseFile(src string, fset *token.FileSet) *ast.File {
@@ -240,24 +269,19 @@ func ScoutFunction(path string, config FunctionConfig) (*FuncNode, error) {
 	}
 
 	fset := token.NewFileSet()
-	inspector := FuncInspector{
-		Nodes:  &[]FuncNode{},
+	inspector := funcInspector{
+		Nodes:  []FuncNode{},
 		Config: config,
-		Base:   BaseInspector{Path: path, Fset: fset},
+		Base:   baseInspector{Path: path, Fset: fset},
 	}
 	node := parseFile(path, fset)
-	ast.Inspect(node, func(n ast.Node) bool {
-		if inspector.Base.Count > 0 {
-			return false
-		}
-		return functionInspector(n, inspector)
-	})
+	inspector.inspect(node)
 
-	if len(*inspector.Nodes) == 0 {
-		err := errors.New("no functions were found based on configuration")
+	if len(inspector.Nodes) == 0 {
+		err := errors.New("no function was found based on configuration")
 		return nil, err
 	}
-	return &(*inspector.Nodes)[0], nil
+	return &(inspector.Nodes)[0], nil
 }
 
 func ScoutStruct(path string, config StructConfig) (*StructNode, error) {
@@ -266,22 +290,17 @@ func ScoutStruct(path string, config StructConfig) (*StructNode, error) {
 	}
 
 	fset := token.NewFileSet()
-	inspector := StructInspector{
-		Nodes:  &[]StructNode{},
+	inspector := structInspector{
+		Nodes:  []StructNode{},
 		Config: config,
-		Base:   BaseInspector{Path: path, Fset: fset},
+		Base:   baseInspector{Path: path, Fset: fset},
 	}
 	node := parseFile(path, fset)
-	ast.Inspect(node, func(n ast.Node) bool {
-		if inspector.Base.Count > 0 {
-			return false
-		}
-		return structInspector(n, inspector)
-	})
+	inspector.inspect(node)
 
-	if len(*inspector.Nodes) == 0 {
-		err := errors.New("no functions were found based on configuration")
+	if len(inspector.Nodes) == 0 {
+		err := errors.New("no function was found based on configuration")
 		return nil, err
 	}
-	return &(*inspector.Nodes)[0], nil
+	return &(inspector.Nodes)[0], nil
 }
